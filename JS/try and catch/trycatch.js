c400f3840e4d0f968e9fe6a2a255dbee8dcbc5f6
SirajAhmedShekh <sirajshekh068@gmail.com>
2025-08-01 00:33:55 +0530

    trycatch

--- JS/try and catch/trycatch.js
+++ JS/try and catch/trycatch.js
@@ -24,46 +24,46 @@
 
 // API fetch consept.
 
-// const api= `https://jsonplaceholder.typicode.com/photos`;
+const api= `https://jsonplaceholder.typicode.com/photos`;
 
-// const Apicall = ()=>{
-//   fetch(api) 
-//   .then((res) => {return res.json()})
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err));
-// };
+const Apicall = ()=>{
+  fetch(api) 
+  .then((res) => {return res.json()})
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+};
 
-// const apicall = async () => {
-//   try{
-//     let respons = await fetch(api);
-//     let data = await respons.json();
-//     console.warn(' ~ apicall ~ data:', data);
-//   } catch (error){
-//     console.warn(' ~ apicall ~ data:', error)
-//   }
-// };
+const apicall = async () => {
+  try{
+    let respons = await fetch(api);
+    let data = await respons.json();
+    console.warn(' ~ apicall ~ data:', data);
+  } catch (error){
+    console.warn(' ~ apicall ~ data:', error)
+  }
+};
 
-// const appendsFunc =(data) => {
-// let datashow = document.getElementById('info');
-// data.forEach((element)=> {
+const appendsFunc =(data) => {
+let datashow = document.getElementById('info');
+data.forEach((element)=> {
 
-//   let cardDiv = document.createElement('div');
-//   let img = document.createElement('img');
-// let id = document.createElement('h3');
-// let = document.createElement('h1')
+  let cardDiv = document.createElement('div');
+  let img = document.createElement('img');
+let id = document.createElement('h3');
+let = document.createElement('h1')
 
 
-// cardDiv.className = 'card_div';
-// img.src = element.thubnailurl;
-// id.innerText = element.id;
-// title.innerText = element.title;
+cardDiv.className = 'card_div';
+img.src = element.thubnailurl;
+id.innerText = element.id;
+title.innerText = element.title;
 
-// cardDiv.append(img,id,title);
+cardDiv.append(img,id,title);
 
-// datashow.append(cardDiv);
+datashow.append(cardDiv);
 
-// });
-// };
+});
+};
 
 
 
