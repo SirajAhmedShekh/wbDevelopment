8f1f6724e8663a690ab460e4648c749ad61c30d9
SirajAhmedShekh <sirajshekh068@gmail.com>
2025-09-01 20:12:01 +0530

    formvalidationcode

--- My Project/pay.js
+++ My Project/pay.js
@@ -1,18 +1,18 @@
+
+
 function paymentFunc(e) {
-    e.preventDefault();
-    let userName = document.getElementById("user");
-    let userEmail = document.getElementById("email");
-    let zipCode = document.getElementById("zipCode");
-    let cardName = document.getElementById("cardName");
-    let cardNumber = document.getElementById("cardNumber");
-    let expMonth = document.getElementById("expMonth");
-    let expYear = document.getElementById("expYear");
-    let cardCvv = document.getElementById("cardCvv");
+  e.preventDefault();
+  let userName = document.getElementById("user").value.trim();
+  let userEmail = document.getElementById("email").value.trim();
+  let zipCode = document.getElementById("zipCode").value.trim();
+  let cardName = document.getElementById("cardName").value.trim();
+  let cardNumber = document.getElementById("cardNumber").value.trim();
+  let cardCvv = document.getElementById("cardCvv").value.trim();
 
 
-// ==== user validation =====
+  // ==== user validation =====
 
-if (userName == "") {
+  if (userName == "") {
     document.getElementById("username_msg").innerHTML = "please fill the username field";
     document.getElementById("username_msg").style.color = "gray";
     return false;
@@ -25,20 +25,20 @@ if (userName == "") {
     document.getElementById("username_msg").style.color = "gray";
     return false;
   } else {
-    document.getElementById("username_msg").innerHTML = ""
+    document.getElementById("username_msg").innerHTML = "";
   }
 
 
-// ==== email Validation ====
+  // ==== email Validation ====
 
   if (userEmail == "") {
     document.getElementById("email_msg").innerHTML =
       " Please fill the email field";
-    document.getElementById("email_msg").style.color = "red";
+    document.getElementById("email_msg").style.color = "gray";
     return false;
   } else if (userEmail.indexOf("@") <= 0) {
     document.getElementById("email_msg").innerHTML = " @ Invalid Position.";
-    document.getElementById("email_msg").style.color = "red";
+    document.getElementById("email_msg").style.color = "gray";
     return false;
   }
   //length 19
@@ -47,16 +47,16 @@ if (userName == "") {
     userEmail.charAt(userEmail.length - 3) != "."
   ) {
     document.getElementById("email_msg").innerHTML = " Invalid Position.";
-    document.getElementById("email_msg").style.color = "red";
+    document.getElementById("email_msg").style.color = "gray";
     return false;
   } else {
-    document.getElementById("email_msg").innerHTML = ""
+    document.getElementById("email_msg").innerHTML = "";
   }
 
-// ==== zip code Validation ====
+  // ==== zip code Validation ====
 
-if (zipCode == "") {
-    document.getElementById("zipCode_msg").innerHTML = "please fill the username field";
+  if (zipCode == "") {
+    document.getElementById("zipCode_msg").innerHTML = "please fill the zipcode field";
     document.getElementById("zipCode_msg").style.color = "gray";
     return false;
   } else if (zipCode.length < 6 || zipCode.length > 6) {
@@ -68,13 +68,13 @@ if (zipCode == "") {
     document.getElementById("zipCode_msg").style.color = "gray";
     return false;
   } else {
-    document.getElementById("zipCode_msg").innerHTML = ""
+    document.getElementById("zipCode_msg").innerHTML = "";
   }
 
   // ==== name on card validation =====
 
-if (cardName == "") {
-    document.getElementById("cardname_msg").innerHTML = "please fill the username field";
+  if (cardName == "") {
+    document.getElementById("cardname_msg").innerHTML = "please fill the cardname field";
     document.getElementById("cardname_msg").style.color = "gray";
     return false;
   } else if (cardName.length <= 2 || cardName.length > 20) {
@@ -86,11 +86,43 @@ if (cardName == "") {
     document.getElementById("cardname_msg").style.color = "gray";
     return false;
   } else {
-    document.getElementById("cardname_msg").innerHTML = ""
+    document.getElementById("cardname_msg").innerHTML = "";
   }
 
-  
 
+  // ==== card number Validation ====
 
+  if (cardNumber == "") {
+    document.getElementById("cardnumber_msg").innerHTML = "please fill the cardnumber field";
+    document.getElementById("cardnumber_msg").style.color = "gray";
+    return false;
+  } else if (cardNumber.length < 16 || cardNumber.length > 16) {
+    document.getElementById("cardnumber_msg").innerHTML = "please enter the length of 16 ";
+    document.getElementById("cardnumber_msg").style.color = "gray";
+    return false;
+  } else if (isNaN(cardNumber)) {
+    document.getElementById("cardnumber_msg").innerHTML = "Only number are allowed";
+    document.getElementById("cardnumber_msg").style.color = "gray";
+    return false;
+  } else {
+    document.getElementById("cardnumber_msg").innerHTML = "";
+  }
 
-}
+  // ==== CVV Validation ====
+
+  if (cardCvv == "") {
+    document.getElementById("cardcvv_msg").innerHTML = "please fill the cardnumber field";
+    document.getElementById("cardcvv_msg").style.color = "gray";
+    return false;
+  } else if (cardCvv.length < 3 || cardCvv.length > 3) {
+    document.getElementById("cardcvv_msg").innerHTML = "please enter the length of 3 ";
+    document.getElementById("cardcvv_msg").style.color = "gray";
+    return false;
+  } else if (isNaN(cardCvv)) {
+    document.getElementById("cardcvv_msg").innerHTML = "Only number are allowed";
+    document.getElementById("cardcvv_msg").style.color = "gray";
+    return false;
+  } else {
+    document.getElementById("cardcvv_msg").innerHTML = "";
+  }
+};
