c125749cc438d1ad430a5b25001d67836d85454b
SirajAhmedShekh <sirajshekh068@gmail.com>
2025-09-07 14:26:23 +0530

    newCode

--- ExercisePractice/index.js
+++ ExercisePractice/index.js
@@ -1,16 +1,31 @@
 // two sum problem
 
-let nums = [2,11,7,15];
-let target = 9;
- function twosum(){
-for(let i=0;i<nums.length;i++){
-    for(let j=1;j<nums.length;j++){
-        if(nums[i]+nums[j]===target){
-return [i,j];
-        }
-    }
-}
-return []
-}
-let ans=twosum();
-console.log(ans);
+// let nums = [2,11,7,15];
+// let target = 9;
+//  function twosum(){
+// for(let i=0;i<nums.length;i++){
+//     for(let j=1;j<nums.length;j++){
+//         if(nums[i]+nums[j]===target){
+// return [i,j];
+//         }
+//     }
+// }
+// return []
+// }
+// let ans=twosum();
+// console.log(ans);
+
+
+// let arr = 121;
+
+// function isPalindrome(x){
+// let newarray = x.toString().split("");
+// let jhola = "";
+// for(let i=newarray.length-1; i>=0;i--){
+//     jhola += newarray[i];
+// }
+// if(jhola == arr) return true; else return false ;
+// }
+// let result = isPalindrome(arr)
+// console.log(result);
+
